8ba8f7ff9487c8d5192a611bf2be3ccc69580e23
tommyrharper <thomasrobertharper@gmail.com>
2020-10-27 23:27:59 +0000

    Add the haveWon() method to auto_balance.rb

--- auto_balance.rb
+++ auto_balance.rb
@@ -3,6 +3,8 @@ class AutoBalancer
   attr_accessor :cash, :btc, :btcPrice, :btcValue
 
   def initialize(initialCash = 1000, initialBtcPrice = 1000, initialBtc = 1)
+    @initialBtc = initialBtc
+    @initialCash = initialCash
     @cash = initialCash
     @btc = initialBtc
     @btcPrice = initialBtcPrice
@@ -27,20 +29,45 @@ class AutoBalancer
       "btc" => @btc,
       "btcPrice" => @btcPrice,
       "btcValue" => @btcValue,
-      "total" => @total
+      "total" => @total,
+      "won" => haveWon()
     }
+  end
+
+  def haveWon()
+    totalHolding = @initialCash + (@initialBtc * @btcPrice)
+    if @total == totalHolding
+      return 'draw'
+    end
+    @total > totalHolding
   end
 
 end
 
 balancer = AutoBalancer.new
 updateValue = 100
+updateValueTwo = 94.85227
 
-for i in 0...1000
-  balancer.updateBtcPrice(updateValue)
-  balancer.updateBtcPrice(-updateValue)
-  balancer.updateBtcPrice(-updateValue)
-  balancer.updateBtcPrice(updateValue)
+
+# for a in 0...1000
+#   balancer.updateBtcPrice(updateValue)
+#   balancer.updateBtcPrice(-updateValueTwo)
+# end
+
+# iterate update value two between 90 and 100
+# return when it wins
+for i in 945000...1000000
+  # puts i
+  puts 100 - (i.to_f/10000)
+  for a in 0...1000
+    balancer.updateBtcPrice(updateValue)
+    balancer.updateBtcPrice(-(i.to_f/10000))
+  end
+  if balancer.haveWon() == true
+    puts 'won'
+    return
+  end
+  balancer = AutoBalancer.new
 end
 
 puts balancer.returnResults()
